1f3c072a62abe0e50c6d10c56e3737b2c3e38b82
Trisha <48178740+trishapelagio@users.noreply.github.com>
2020-09-07 17:54:25 +0800

    added the db model templates

---
+++ models/OrderModel.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var OrderSchema = new mongoose.Schema({
+    //put variables, i only put samples to be followed
+    listingOwner: {
+        type: String,
+        required: true
+    },
+    productType: {
+        type: String,
+        enum: ['clothes', 'food', 'electronics', 'tickets', 
+                'furniture', 'beauty', 'books', 'hobbies',
+                'sports', 'accessories', 'media', 'music', 'pets'],
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive'],
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Order', OrderSchema);
