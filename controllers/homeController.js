405658bcc2c6a28ba09150c687eaf74a3e86db90
michaeladizon <michaela_dizon@dlsu.edu.ph>
2020-09-12 18:05:14 +0800

    order details

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,18 @@
+const db = require('../models/db.js');
+const Product = require('../models/ProductModel.js')
+
 const homeController = {
 	home: function(req,res){
-		res.render('home');
+		var projection = 'name price'
+
+		db.findMany(Product, null, projection, function(results) {
+			console.log(results)
+			if(results != null) {
+				res.render('home', {products: results})
+			}
+		})
+
+		// res.render('home');
 	}
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -39,6 +39,7 @@ app.get('/orderPending', orderPendingController.orderPending);
 app.get('/privacy', privacyController.privacy);
 app.get('/productDetails', productDetailsController.productDetails);
 app.post('/addToCart',shoppingCartController.addToCart);
+app.post('/postDetails', shoppingCartController.postDetails);
 // app.post('/removeItem/:id',shoppingCartController.removeItem);
 app.get('/search', searchController.search);
 app.get('/shippingDetails', shippingDetailsController.shippingDetails);
