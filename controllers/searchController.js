0ba92433a118f1710c87dcde520ede9260ba8b41
michaeladizon <michaela_dizon@dlsu.edu.ph>
2020-09-19 18:02:20 +0800

    search

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,6 +1,14 @@
+const db = require('../models/db.js');
+const Product = require('../models/ProductModel.js')
+
 const searchController = {
 	search: function(req,res){
-		res.render('search');
+		var input = req.query.searchInput
+		var query = {name: {$regex: input, "$options": "i"}}
+
+		db.findMany(Product, query, null, function(result) {
+			res.render('search', {products: result})
+		})
 	}
 }
 
