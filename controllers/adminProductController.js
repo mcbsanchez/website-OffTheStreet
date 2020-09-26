008155885dc4845cc031fcbfd352846773d2e305
Trisha <48178740+trishapelagio@users.noreply.github.com>
2020-09-26 21:55:28 +0800

    editted admin view

--- controllers/adminProductController.js
+++ controllers/adminProductController.js
@@ -1,6 +1,17 @@
+const db = require('../models/db.js');
+const Product = require('../models/ProductModel.js')
+
 const adminProductController = {
 	adminProduct: function(req,res){
-		res.render('admin-product-page');
+		db.findMany(Product, null, null, function(results) {
+			products = results;
+			for(var i=0 ; i<results.length ; i++){
+				products[i].number = i+1;
+			}
+			if(results != null) {
+				res.render('admin-product-page', {products: products})
+			}
+		})
 	}
 }
 
